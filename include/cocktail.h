dc2208d8c507dd007b5bba0ac931d4c58381685b
Grégoire Astruc <gregoire.astruc@gmail.com>
2013-02-04 21:58:07 +0100

    Missing name_ field.

--- include/cocktail.h
+++ include/cocktail.h
@@ -11,6 +11,9 @@ class Cocktail : public QObject
     Q_TESTCLASS(Cocktail)
 public:
     explicit Cocktail(const QString &name, QObject *parent = 0);
+    
+protected:
+    QString name_;
 };
 
 #endif // COCKTAIL_H
